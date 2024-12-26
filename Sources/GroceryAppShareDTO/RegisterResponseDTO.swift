f0f40dbdaaba6d209b36ac69e77ec8e8f4e80615
Ancel Guarayo <aguarayo@gmail.com>
2024-12-26 23:52:42 +0100

    First commit

---
+++ Sources/GroceryAppShareDTO/RegisterResponseDTO.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterResponseDTO.swift
+//  GroceryAppShareDTO
+//
+//  Created by Ancel Dev account on 26/12/24.
+//
+
+import Foundation
+
+public struct RegisterResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+    
+    public init(error: Bool, reason: String? = nil) {
+        self.error = error
+        self.reason = reason
+    }
+}
