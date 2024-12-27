75b12f20a835508937450d8312d3cc795c31bd0b
Ancel Guarayo <aguarayo@gmail.com>
2024-12-27 01:08:49 +0100

    Added Sendable to Login and Register

--- Sources/GroceryAppShareDTO/LoginResponseDTO.swift
+++ Sources/GroceryAppShareDTO/LoginResponseDTO.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct LoginResponseDTO: Codable {
+public struct LoginResponseDTO: Codable, @unchecked Sendable {
     public let error: Bool
     public var reason: String? = nil
     public var token: String? = nil
--- Sources/GroceryAppShareDTO/RegisterResponseDTO.swift
+++ Sources/GroceryAppShareDTO/RegisterResponseDTO.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct RegisterResponseDTO: Codable {
+public struct RegisterResponseDTO: Codable, @unchecked Sendable {
     public let error: Bool
     public var reason: String? = nil
     
